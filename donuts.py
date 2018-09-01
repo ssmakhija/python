a03e6c91c9d0a69893d00e42aaf02f9252ff75f1
ssmakhija <42876083+ssmakhija@users.noreply.github.com>
2018-09-01 17:54:34 +0530

    Delete donuts.py

--- donuts.py
+++
@@ -1,9 +0,0 @@
-x=eval(input("Enter 1 Number"))
-if x<1:
-	print("invalid input")
-elif x<=2:
-	print("too few donuts")
-elif x<11:
-	print("Number of Donuts "+str(x))
-else:
-	print("No of donuts too many")
